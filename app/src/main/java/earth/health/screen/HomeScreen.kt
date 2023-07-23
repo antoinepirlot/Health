81d15ce603e8e2777b016b206f5b544ed3774d64
Antoine <pirlotantoine2@gmail.com>
2023-07-23 22:21:34 +0200

    Update : align centered boxes

--- app/src/main/java/earth/health/screen/HomeScreen.kt
+++ app/src/main/java/earth/health/screen/HomeScreen.kt
@@ -1,8 +1,9 @@
 package earth.health.screen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import earth.health.R
@@ -10,7 +11,7 @@ import earth.health.ui.theme.HealthTheme
 
 @Composable
 fun HomeScreen() {
-    Column() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Card(title = stringResource(id = R.string.food), text = "1920/1950 kcal", button = {})
         Card(title = stringResource(id = R.string.weight), text = "80 kg", button = {})
     }
