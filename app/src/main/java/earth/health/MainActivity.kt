7ef208c85eb20c59ea5a65bb1c3b2bc297551074
Antoine <pirlotantoine2@gmail.com>
2023-07-23 22:29:29 +0200

    Add: nav controller

--- app/src/main/java/earth/health/MainActivity.kt
+++ app/src/main/java/earth/health/MainActivity.kt
@@ -3,6 +3,9 @@ package earth.health
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import earth.health.screen.HomeScreen
 import earth.health.ui.theme.HealthTheme
 
@@ -10,8 +13,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             HealthTheme {
-                HomeScreen()
+                NavHost(navController = navController, startDestination = "/home") {
+                    composable("/home") {
+                        HomeScreen()
+                    }
+                }
             }
         }
     }
