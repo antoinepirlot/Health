c7d916ffc4915ec5c1f3616e5b3b16289c03d1f9
Antoine <pirlotantoine2@gmail.com>
2023-07-23 22:13:47 +0200

    Update : use stored strings instead of hard coded strings

--- app/src/main/java/earth/health/screen/HomeScreen.kt
+++ app/src/main/java/earth/health/screen/HomeScreen.kt
@@ -1,12 +1,19 @@
 package earth.health.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import earth.health.R
 import earth.health.ui.theme.HealthTheme
 
 @Composable
 fun HomeScreen() {
-    Card(title = "Nourriture", text = "1920/1950 kcal", button = {})
+    Column() {
+        Card(title = stringResource(id = R.string.food), text = "1920/1950 kcal", button = {})
+        Card(title = stringResource(id = R.string.weight), text = "80 kg", button = {})
+    }
 }
 
 @Preview()
