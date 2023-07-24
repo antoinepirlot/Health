c8dd746cc4b50825237b1fb17d1ea358cfe68ed6
Antoine <pirlotantoine2@gmail.com>
2023-07-24 20:27:13 +0200

    Add: meal page screen composable

---
+++ app/src/main/java/earth/health/screen/meal/MealHomeScreen.kt
@@ -0,0 +1,24 @@
+package earth.health.screen.meal
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import earth.health.R
+import earth.health.screen.Card
+
+@Composable
+fun MealHomeScreen() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Card(title = stringResource(id = R.string.breakfast), text = "null", button = {})
+        Card(title = stringResource(id = R.string.lunch), text = "null", button = {})
+        Card(title = stringResource(id = R.string.dinner), text = "null", button = {})
+    }
+}
+
+@Preview
+@Composable
+fun MealHomeScreenPreview() {
+    MealHomeScreen()
+}
