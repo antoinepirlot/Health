10c6726a9d69a1ce338c2d9043ab7d3fe2a92026
Antoine <pirlotantoine2@gmail.com>
2023-07-24 21:08:30 +0200

    Fix : add router import

--- app/src/main/java/earth/health/MainActivity.kt
+++ app/src/main/java/earth/health/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import earth.health.router.Router
 import earth.health.screens.HomeScreen
 import earth.health.ui.theme.HealthTheme
 
