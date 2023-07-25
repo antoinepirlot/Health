810ca28d35a7c38720412db4d15164082c1ea8fe
Antoine <pirlotantoine2@gmail.com>
2023-07-25 22:16:06 +0200

    Add: Meal screen

--- app/src/main/java/earth/health/screens/meal/MealScreen.kt
+++ app/src/main/java/earth/health/screens/meal/MealScreen.kt
@@ -1,9 +1,34 @@
 package earth.health.screens.meal
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import earth.health.data.entity.Meal
+import earth.health.data.entity.getDefaultFood
+import earth.health.data.entity.getDefaultMeals
 
 @Composable
 fun MealScreen(meal: Meal) {
+    Column {
+        Text(text = meal.name)
+        for (food in getDefaultFood()) {
+            TextButton(onClick = { /*TODO*/ }) {
+                //fillMaxWidth makes the button clickable on all the width even on blank
+                Text(text = food.name, modifier = Modifier.fillMaxWidth())
+            }
+            Divider()
+        }
+    }
+}
 
+@Preview
+@Composable
+fun MealScreenPreview() {
+    MealScreen(meal = getDefaultMeals()[0])
 }
