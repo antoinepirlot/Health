812a3d820a29266dc9b48137e2f218b09d8b69c7
Antoine <pirlotantoine2@gmail.com>
2023-07-26 23:32:50 +0200

    Update: add column info for each var

--- app/src/main/java/earth/health/data/entity/Food.kt
+++ app/src/main/java/earth/health/data/entity/Food.kt
@@ -1,20 +1,21 @@
 package earth.health.data.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class Food(
-    @PrimaryKey (autoGenerate = true) var id: Long = 0,
-    var name: String,
-    var kcal: Int = 0,
-    var lipids: Double = 0.0,
-    var saturated_lipids: Double = 0.0,
-    var carbohydrates: Double = 0.0,
-    var sugar: Double = 0.0,
-    var protein: Double = 0.0,
-    var alimentary_fiber: Double = 0.0,
-    var calcium: Double = 0.0
+    @PrimaryKey(autoGenerate = true) var id: Long = 0,
+    @ColumnInfo(name = "name") var name: String,
+    @ColumnInfo(name = "kcal") var kcal: Int = 0,
+    @ColumnInfo(name = "lipids") var lipids: Double = 0.0,
+    @ColumnInfo(name = "saturated_lipids") var saturatedLipids: Double = 0.0,
+    @ColumnInfo(name = "carbohydrates") var carbohydrates: Double = 0.0,
+    @ColumnInfo(name = "sugar") var sugar: Double = 0.0,
+    @ColumnInfo(name = "protein") var protein: Double = 0.0,
+    @ColumnInfo(name = "alimentary_fiber") var alimentaryFiber: Double = 0.0,
+    @ColumnInfo(name = "calcium") var calcium: Double = 0.0
 )
 
 fun getDefaultFood() = listOf<Food>(
