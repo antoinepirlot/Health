720142b8196f48f876b960b61f373e9d631600cb
Antoine <pirlotantoine2@gmail.com>
2023-07-26 23:06:42 +0200

    Add: column info annotation + change day object to int and add it as foreign key

--- app/src/main/java/earth/health/data/entity/Meal.kt
+++ app/src/main/java/earth/health/data/entity/Meal.kt
@@ -2,20 +2,21 @@ package earth.health.data.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.time.LocalDate
 
-@Entity
+@Entity(foreignKeys = [ForeignKey(entity = Day::class, parentColumns = ["id"], childColumns = ["day"],onDelete = ForeignKey.CASCADE)])
 data class Meal(
     @PrimaryKey(autoGenerate = true) var id: Long = 0,
-    var name: Meals,
-    var foodList: List<Food> = listOf(),
-    var date: LocalDate = LocalDate.now(),
-    var totalKcal: Int = computeTotalKcal(foodList)
+    @ColumnInfo(name = "name") var name: Meals,
+    @ColumnInfo(name = "foodList") var foodList: List<Food> = listOf(),
+    @ColumnInfo(name = "day") var day: Int,
+    @ColumnInfo(name = "totalKcal") var totalKcal: Int = computeTotalKcal(foodList)
 )
 
 fun getDefaultMeals() = arrayListOf<Meal>(
-    Meal(name = Meals.BREAKFAST)
+    Meal(name = Meals.BREAKFAST, day = 0)
 )
 
 private fun computeTotalKcal(foodList: List<Food>): Int {
