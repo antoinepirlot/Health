c4dcf6a0912bb9204c58fccc9ff612d63ede2fae
Antoine <pirlotantoine2@gmail.com>
2023-07-27 23:34:38 +0200

    Optimise: remove unused import

--- app/src/main/java/earth/health/MainActivity.kt
+++ app/src/main/java/earth/health/MainActivity.kt
@@ -5,11 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
-import earth.health.data.view_models.DayViewModel
-import earth.health.data.view_models.FoodMealRelViewModel
-import earth.health.data.view_models.FoodViewModel
-import earth.health.data.view_models.MealViewModel
 import earth.health.router.Router
 import earth.health.ui.theme.HealthTheme
 
