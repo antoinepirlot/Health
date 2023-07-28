7d9cd209685cc8b9cec9b9f7230e5d04b6e67bcf
Antoine Pirlot <pirlotantoine2@gmail.com>
2023-07-28 10:14:53 +0200

    Fix: change table name to fix reading data to unexisting table (because "Meal" is different from "meal"

--- app/src/main/java/earth/health/data/entity/Day.kt
+++ app/src/main/java/earth/health/data/entity/Day.kt
@@ -6,7 +6,7 @@ import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.time.LocalDate
 
-@Entity(indices = [Index(value = ["date"], unique = true)])
+@Entity(tableName = "day", indices = [Index(value = ["date"], unique = true)])
 data class Day(
     @PrimaryKey(autoGenerate = true) var id: Long = 0,
     @ColumnInfo(name = "date") var date: LocalDate = LocalDate.now(),
--- app/src/main/java/earth/health/data/entity/Meal.kt
+++ app/src/main/java/earth/health/data/entity/Meal.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "meal")
 data class Meal(
     @PrimaryKey(autoGenerate = true) var id: Long = 0,
     @ColumnInfo(name = "name") var name: Meals,
