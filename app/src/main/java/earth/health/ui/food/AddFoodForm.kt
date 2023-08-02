54258bddfbfc3eeb2630fd229bc11d564c68a3d1
Antoine <pirlotantoine2@gmail.com>
2023-08-02 21:40:36 +0200

    Refactor: move all form to form compose func

--- app/src/main/java/earth/health/ui/food/AddFoodForm.kt
+++ app/src/main/java/earth/health/ui/food/AddFoodForm.kt
@@ -1,107 +1,133 @@
 package earth.health.ui.food
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import earth.health.R
+import earth.health.data.entity.Food
+import earth.health.data.view_models.FoodViewModel
 
 @Composable
 fun AddFoodForm(
     modifier: Modifier = Modifier,
-    name: String,
-    onChangeName: (String) -> Unit,
-    kcal: String,
-    onChangeKcal: (String) -> Unit,
-    lipids: String,
-    onChangeLipids: (String) -> Unit,
-    saturatedLipids: String,
-    onChangeSaturatedLipids: (String) -> Unit,
-    carbohydrates: String,
-    onChangeCarbohydrates: (String) -> Unit,
-    sugar: String,
-    onChangeSugar: (String) -> Unit,
-    protein: String,
-    onChangeProtein: (String) -> Unit,
-    alimentaryFiber: String,
-    onChangeAlimentaryFiber: (String) -> Unit,
-    calcium: String,
-    onChangeCalcium: (String) -> Unit,
 ) {
-    Column(modifier = modifier) {
+    val foodViewModel = viewModel<FoodViewModel>()
+    var foodName by rememberSaveable { mutableStateOf("") }
+    var kcal by rememberSaveable { mutableStateOf("0") }
+    var lipids by rememberSaveable { mutableStateOf("0.0") }
+    var saturatedLipids by rememberSaveable { mutableStateOf("0.0") }
+    var carbohydates by rememberSaveable { mutableStateOf("0.0") }
+    var sugar by rememberSaveable { mutableStateOf("0.0") }
+    var protein by rememberSaveable { mutableStateOf("0.0") }
+    var alimentaryFiber by rememberSaveable { mutableStateOf("0.0") }
+    var calcium by rememberSaveable { mutableStateOf("0.0") }
+
+    val toast = Toast.makeText(
+        LocalContext.current,
+        stringResource(id = R.string.add_food_succeed),
+        Toast.LENGTH_SHORT
+    )
+
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
         // todo try to simplify
         OutlinedTextField(
             label = { Text(text = stringResource(id = R.string.food_name_form)) },
-            value = name,
-            onValueChange = onChangeName
+            value = foodName,
+            onValueChange = { newFoodName -> foodName = newFoodName },
         )
         OutlinedTextField(
             label = { Text(text = "kcal") },
             value = kcal,
-            onValueChange = onChangeKcal
+            onValueChange = { newFoodKcal -> kcal = newFoodKcal }
         )
         OutlinedTextField(
             label = { Text(text = stringResource(id = R.string.lipids)) },
             value = lipids,
-            onValueChange = onChangeLipids
+            onValueChange = { newFoodLipids -> lipids = newFoodLipids }
         )
         OutlinedTextField(
             label = { Text(text = stringResource(id = R.string.saturated_lipids)) },
             value = saturatedLipids,
-            onValueChange = onChangeSaturatedLipids
+            onValueChange = { newFoodSaturatedLipids ->
+                saturatedLipids = newFoodSaturatedLipids
+            }
         )
         OutlinedTextField(
             label = { Text(text = stringResource(id = R.string.carbohydrates)) },
-            value = carbohydrates,
-            onValueChange = onChangeCarbohydrates
+            value = carbohydates,
+            onValueChange = { newFoodCarbohydrates ->
+                carbohydates = newFoodCarbohydrates
+            }
         )
         OutlinedTextField(
             label = { Text(text = stringResource(id = R.string.sugar)) },
             value = sugar,
-            onValueChange = onChangeSugar
+            onValueChange = { newFoodSugar -> sugar = newFoodSugar }
         )
         OutlinedTextField(
             label = { Text(text = stringResource(id = R.string.protein)) },
             value = protein,
-            onValueChange = onChangeProtein
+            onValueChange = { newFoodProtein -> protein = newFoodProtein }
         )
         OutlinedTextField(
             label = { Text(text = stringResource(id = R.string.alimentary_fiber)) },
             value = alimentaryFiber,
-            onValueChange = onChangeAlimentaryFiber
+            onValueChange = { newFoodAlimentaryFiber ->
+                alimentaryFiber = newFoodAlimentaryFiber
+            }
         )
         OutlinedTextField(
             label = { Text(text = stringResource(id = R.string.calcium)) },
             value = calcium,
-            onValueChange = onChangeCalcium
+            onValueChange = { newFoodCalcium -> calcium = newFoodCalcium }
         )
+        Button(
+            onClick = {
+                val food = Food(
+                    name = foodName,
+                    kcal = kcal.toInt(),
+                    lipids = lipids.toDouble(),
+                    saturatedLipids = saturatedLipids.toDouble(),
+                    carbohydrates = carbohydates.toDouble(),
+                    sugar = sugar.toDouble(),
+                    protein = protein.toDouble(),
+                    alimentaryFiber = alimentaryFiber.toDouble(),
+                    calcium = calcium.toDouble()
+                )
+                foodViewModel.createFood(food)
+                foodName = ""
+                kcal = "0"
+                lipids = "0.0"
+                saturatedLipids = "0.0"
+                carbohydates = "0.0"
+                sugar = "0.0"
+                protein = "0.0"
+                alimentaryFiber = "0.0"
+                calcium = "0.0"
+                toast.show()
+            }
+        ) {
+            Text(text = stringResource(id = R.string.add))
+        }
     }
 }
 
 @Preview
 @Composable
 fun AddFoodFormPreview() {
-    AddFoodForm(
-        name = "",
-        onChangeName = {},
-        kcal = "",
-        onChangeKcal = {},
-        lipids = "",
-        onChangeLipids = {},
-        saturatedLipids = "",
-        onChangeSaturatedLipids = {},
-        carbohydrates = "",
-        onChangeCarbohydrates = {},
-        sugar = "",
-        onChangeSugar = {},
-        protein = "",
-        onChangeProtein = {},
-        alimentaryFiber = "",
-        onChangeAlimentaryFiber = {},
-        calcium = "",
-        onChangeCalcium = {} 
-    )
+    AddFoodForm()
 }
