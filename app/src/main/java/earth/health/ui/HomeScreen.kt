f31f06c61b2e13d8f111e66e665a769114af3dbd
Antoine <pirlotantoine2@gmail.com>
2023-08-02 21:32:31 +0200

    Remove: editor automatic changes

--- app/src/main/java/earth/health/ui/HomeScreen.kt
+++ app/src/main/java/earth/health/ui/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -18,43 +19,35 @@ import earth.health.router.Destination
 fun HomeScreen(navController: NavController, days: List<Day>) {
     if (days.isNotEmpty()) {
         val latestDay = days.last()
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            content = function(latestDay, navController)
-        )
-    }
-}
-
-@Composable
-private fun function(
-    latestDay: Day,
-    navController: NavController
-): @Composable() (ColumnScope.() -> Unit) =
-    {
         val kcalText = latestDay.totalKcal.toString() + "/1920kcal"
-        Card(
-            title = stringResource(id = R.string.food),
-            text = kcalText,
-            mainAction = { navController.navigate(Destination.MEALS.link) },
-            fastAction = {})
-        Card(
-            title = stringResource(id = R.string.weight),
-            text = "80 kg",
-            mainAction = { navController.navigate(Destination.WEIGHT.link) },
-            fastAction = {})
-        Card(
-            title = stringResource(id = R.string.add_food_screen),
-            text = "",
-            mainAction = { navController.navigate(Destination.ADD_FOOD_SCREEN.link) }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Card(
+                title = stringResource(id = R.string.food),
+                text = kcalText,
+                mainAction = { navController.navigate(Destination.MEALS.link) },
+                fastAction = {})
+            Card(
+                title = stringResource(id = R.string.weight),
+                text = "80 kg",
+                mainAction = { navController.navigate(Destination.WEIGHT.link) },
+                fastAction = {})
+            Card(
+                title = stringResource(id = R.string.add_food_screen),
+                text = "",
+                mainAction = { navController.navigate(Destination.ADD_FOOD_SCREEN.link) }
+            ) {
 
+            }
         }
     }
+}
 
 @Preview
 @Composable
 fun HomePreview() {
     HealthTheme {
-        HomeScreen(rememberNavController(), viewModel())
+        HomeScreen(rememberNavController(), listOf())
     }
 }
