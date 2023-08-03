2902b58a7289102db853054677257cd06b1bb6d8
Antoine Pirlot <pirlotantoine2@gmail.com>
2023-08-03 22:27:10 +0200

    Update: center text

--- app/src/main/java/earth/health/ui/meal/MealScreen.kt
+++ app/src/main/java/earth/health/ui/meal/MealScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,8 +17,8 @@ import earth.health.data.entity.Meals
 import earth.health.data.entity.relations.MealWithFoods
 
 @Composable
-fun MealScreen(mealWithFoods: MealWithFoods) {
-    Column {
+fun MealScreen(modifier: Modifier = Modifier, mealWithFoods: MealWithFoods) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = stringResource(id = mealWithFoods.meal.name.mealNameId))
         if (mealWithFoods.foods.isEmpty()) {
             Text(text = stringResource(id = R.string.nothing))
