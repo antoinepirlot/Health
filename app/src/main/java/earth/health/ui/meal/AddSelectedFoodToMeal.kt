f74c945b49a10e27a6257bcd9ae5dce7687e0244
Antoine Pirlot <pirlotantoine2@gmail.com>
2023-08-07 23:18:23 +0200

    Optimise: remove unused imports

--- app/src/main/java/earth/health/ui/meal/AddSelectedFoodToMeal.kt
+++ app/src/main/java/earth/health/ui/meal/AddSelectedFoodToMeal.kt
@@ -6,10 +6,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
