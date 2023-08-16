0f6673cd16f04fffe6439648b82f6d216add6f50
Antoine <pirlotantoine2@gmail.com>
2023-08-17 00:17:27 +0200

    Optimise: remove double verification

--- app/src/main/java/earth/health/data/view_models/DayViewModel.kt
+++ app/src/main/java/earth/health/data/view_models/DayViewModel.kt
@@ -20,12 +20,12 @@ class DayViewModel(application: Application): AndroidViewModel(application) {
         viewModelScope.launch {
             val dbDays = dayDAO.getAll()
             if (dbDays.isEmpty()) {
-                startNewDay()
+                create()
                 return@launch
             }
             days.addAll(dbDays)
             if (dbDays.last().date.isBefore(LocalDate.now())) {
-                startNewDay()
+                create()
             }
         }
     }
