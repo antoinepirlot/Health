c263e8fff9800169af77e8b19e710b50b1748df4
Antoine <pirlotantoine2@gmail.com>
2023-08-18 22:39:24 +0200

    Refactor: move add food form to utils folder

--- app/src/main/java/earth/health/ui/food/AddFoodScreen.kt
+++ app/src/main/java/earth/health/ui/food/AddFoodScreen.kt
@@ -1,26 +1,16 @@
 package earth.health.ui.food
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import earth.health.R
-import earth.health.data.entity.Food
 import earth.health.data.view_models.FoodViewModel
+import earth.health.ui.utils.AddFoodForm
 
 @Composable
 fun AddFoodScreen(
--- app/src/main/java/earth/health/ui/utils/AddFoodForm.kt
+++ app/src/main/java/earth/health/ui/utils/AddFoodForm.kt
@@ -1,4 +1,4 @@
-package earth.health.ui.food
+package earth.health.ui.utils
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
