396bee319a5df172f1d3a21c43ec99b821307f6f
Antoine <pirlotantoine2@gmail.com>
2023-08-19 21:12:18 +0200

    Update: return  mutable object of day after created new one

--- app/src/main/java/earth/health/data/view_models/DayViewModel.kt
+++ app/src/main/java/earth/health/data/view_models/DayViewModel.kt
@@ -1,13 +1,16 @@
 package earth.health.data.view_models
 
 import android.app.Application
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import earth.health.data.HealthDatabase
 import earth.health.data.entity.Day
 import earth.health.data.entity.Meal
 import earth.health.data.entity.Meals
+import earth.health.data.entity.getBlankDay
 import earth.health.data.entity.relations.DayWithMeals
 import kotlinx.coroutines.launch
 import java.lang.IllegalStateException
@@ -32,15 +35,17 @@ class DayViewModel(application: Application): AndroidViewModel(application) {
     /**
      * Create a new day and add it to the days list
      */
-    fun startNewDay() {
+    fun startNewDay(): MutableState<Day> {
         if (days.isNotEmpty() && days.last().date.isEqual(LocalDate.now())) {
-            return
+            throw IllegalStateException("You can't create concurrent days for the same date")
         }
+        val dayToReturn = mutableStateOf(getBlankDay())
         viewModelScope.launch {
             val newDay = Day()
             days.add(newDay)
             newDay.id = nextId()
-            val meals = listOf<Meal>(
+                dayToReturn.value = newDay
+            val meals = listOf(
                 Meal(name = Meals.BREAKFAST, dayId = newDay.id),
                 Meal(name = Meals.LUNCH, dayId = newDay.id),
                 Meal(name = Meals.DINNER, dayId = newDay.id),
@@ -52,6 +57,7 @@ class DayViewModel(application: Application): AndroidViewModel(application) {
                 dayDAO.insertMeal(meal)
             }
         }
+        return dayToReturn
     }
 
     fun getOne(meal: Meal): Day {
