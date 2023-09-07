346806358a28cf20ef4d6c9a67c7ed17b1faff8e
Antoine <pirlotantoine2@gmail.com>
2023-09-07 15:55:43 +0200

    Update: show all information on food screen

--- app/src/main/java/earth/health/ui/food/FoodScreen.kt
+++ app/src/main/java/earth/health/ui/food/FoodScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import earth.health.R
 import earth.health.data.view_models.FoodViewModel
 
 @Composable
@@ -18,5 +20,13 @@ fun FoodScreen(
         modifier = modifier.fillMaxSize()
     ) {
         Text(text = food.name)
+        Text(text = "${food.kcal} kcal")
+        Text(text = stringResource(id = R.string.carbohydrates) + ": ${food.carbohydrates} g")
+        Text(text = stringResource(id = R.string.sugar) + ": ${food.sugar} g")
+        Text(text = stringResource(id = R.string.lipids) + ": ${food.lipids} g")
+        Text(text = stringResource(id = R.string.saturated_lipids) + ": ${food.saturatedLipids} g")
+        Text(text = stringResource(id = R.string.protein) + ": ${food.protein} g")
+        Text(text = stringResource(id = R.string.alimentary_fiber) + ": ${food.alimentaryFiber} g")
+        Text(text = stringResource(id = R.string.calcium) + ": ${food.calcium} g")
     }
 }
